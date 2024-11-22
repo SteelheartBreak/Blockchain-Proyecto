75e977b1c3aa7784ef5b64ecffd0f7225786b8e0
NicoPedraza2001 <nicopedraza7@gmail.com>
2024-11-22 12:00:01 -0500

    Agregacion de clase NFT

--- contracts/SimpleContract.sol
+++ contracts/SimpleContract.sol
@@ -2,9 +2,14 @@
 
 pragma solidity ^0.8.26;
 
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+
 contract SimpleContract {
     uint public data;
-
+    
     constructor() {
         data=2024;
     }
